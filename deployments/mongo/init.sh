b3478c42b20705f7a665508e27c0eacafbadb7ba
TaylorC19 <96517792+TaylorC19@users.noreply.github.com>
2025-07-13 21:35:31 +0900

    feat mongo db initiation and config creation

---
+++ deployments/mongo/init.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+MONGO_ADMIN_USER="root"
+MONGO_ADMIN_PASSWORD="password"
+
+APP_USER="mongo"
+APP_PASSWORD="password"
+DB_NAME="media-library"
+
+mongosh admin -u ${MONGO_ADMIN_USER} -p ${MONGO_ADMIN_PASSWORD} --eval "
+    if (!db.isMaster().ismaster) {
+        rs.initiate(
+            {
+                _id: 'rs0',
+                members: [
+                    {_id: 0, host: 'localhost:27017'}
+                ]
+            }
+        );
+    }
+"
+
+mongosh admin -u ${MONGO_ADMIN_USER} -p ${MONGO_ADMIN_PASSWORD} --eval "
+    var userExists = db.getUser('${APP_USER}');
+
+    if (!userExists) {
+        db.createUser({
+            user: '${APP_USER}',
+            pwd: '${APP_PASSWORD}',
+            roles: [
+                {role: 'readWrite', db: '${DB_NAME}'}
+            ]
+        });
+    } else {
+        db.grantRolesToUser(
+            '${APP_USER}',
+            [
+                {role: 'readWrite', db: '${DB_NAME}'}
+            ]
+        );
+    }
+"
+
+if [ $? -eq 0 ]; then
+    echo "User ${APP_USER} created/updated"
+else
+    echo "could not create user ${APP_USER}"
+fi
